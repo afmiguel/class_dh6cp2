de0f1751b6aff07e432a4446fd683749482e2dba
Afonso Miguel <afonso.miguel@pucpr.br>
2025-10-27 17:20:21 -0300

    Update from professor

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,22 @@ use std::rc::Rc;
 fn main() {
     let shared_data = Rc::new(String::from("Olá mundo"));
     println!("Original: {}[{:p}]", shared_data, shared_data.as_ptr());
+    println!("Contador: {}", Rc::strong_count(&shared_data));
 
     println!("---");
 
     let listener1 = Rc::clone(&shared_data);
     println!("listener1: {}[{:p}]", listener1, listener1.as_ptr());
+    println!("Contador: {}", Rc::strong_count(&listener1));
 
     println!("---");
 
     {
         let listener2 = Rc::clone(&listener1);
         println!("listener2: {}[{:p}]", listener2, listener2.as_ptr());
+        println!("Contador: {}", Rc::strong_count(&listener2));
     }
 
     println!("---");
+    println!("Contador: {}", Rc::strong_count(&shared_data));
 }
