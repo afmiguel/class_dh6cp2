b361a0ce10612801056b01bf4bc71b15d5b9a446
Afonso Miguel <afonso.miguel@pucpr.br>
2025-10-27 17:10:21 -0300

    Update from professor

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use std::rc::Rc;
 
 fn main() {
     let shared_data = Rc::new(String::from("Olá mundo"));
-    println!("Original: {}", shared_data);
+    println!("Original: {}[{:p}]", shared_data, shared_data.as_ptr());
 
     println!("---");
 
     let listener1 = Rc::clone(&shared_data);
-    println!("listener1: {}", listener1);
+    println!("listener1: {}[{:p}]", listener1, listener1.as_ptr());
 }
